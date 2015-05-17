aac1689539dfe1fda00405b3d9ec72f925b7d381
Matthew Sun <matthewkol186@gmail.com>
2015-05-16 23:58:36 -0400

    I HOPE THIS FreAkING WORKS :( WORK IMAGE API TEXT v30

--- www/js/services.js
+++ www/js/services.js
@@ -200,7 +200,8 @@ angular.module('starter.services', ['firebase', 'ngCordova'], function($httpProv
       }).then(function(result){
                 alert(JSON.stringify(result));
                 alert(JSON.stringify(result['response']));
-                id = result['response']['data']['file_id'];
+                alert(JSON.stringify(result['response']['data']['file_id']));
+                id = JSON.stringify(result['response']['data']['file_id']);
                 alert(id);
                 $http.get('http://api.newocr.com/v1/ocr?key='+key+'&file_id'+id+'&page=1&lang=eng&psm=3').
                 success(function(data, status, headers, config) {
