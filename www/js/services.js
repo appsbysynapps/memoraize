bc482b515b6812efee017a912f1720c8663d40b0
Matthew Sun <matthewkol186@gmail.com>
2015-05-16 20:53:35 -0400

    API service should now be completed.

--- www/js/services.js
+++ www/js/services.js
@@ -136,135 +136,22 @@ angular.module('starter.services', ['firebase'], function($httpProvider){
   }
 })
 
-.factory('Reviews', function($firebase) {
-  // Might use a resource here that returns a JSON array
-  var ref = new Firebase("https://devour.firebaseio.com/reviews");
-  var sync = $firebase(ref);
-  // download the data into a local object
-  var syncObject = sync.$asObject();
-  //syncObject.$bindTo($scope, "quizzes");
-
+.factory("TextRazorAPI", function($http) {
   return {
-    all: function() {
-      return sync.$asArray();
-    },
-    getContent: function(reviewId) {
-      var ref2 = ref.child(reviewId+"/content");
-      var str = 0;
-      ref2.once("value", function(data) {
-          str = data.val();
-      });
-        
-      return str;
-    },
-    getRating: function(reviewId) {
-      var ref2 = ref.child(reviewId+"/rating");
-      var rat = 0
-      ref2.once("value", function(data) {
-          rat = data.val();
-      });
-        
-      return rat;
-    },
-    getText: function(reviewId) {
-      var ref2 = ref.child(reviewId+"/content");
-      var str = ""
-      ref2.once("value", function(data) {
-          str = data.val();
-      });
-      return str;
-    },
-    get: function(reviewId) {
-      var ref2 = new Firebase("https://devour.firebaseio.com/reviews/"+reviewId);
-      return $firebase(ref2).$asObject();
-    },
-    add: function(object) {
-      return sync.$push(object);
-    },
-  }
-})
-
-.factory("MyYelpAPI", function($http) {
-  function randomString(length, chars) {
-    var result = '';
-    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
-    return result;
-  }
-  return {
-      "retrieveYelp": function(name, callback, callbackint) {
-          var method = 'GET';
-          var url = 'http://api.yelp.com/v2/search';
-          var params = {
-                  callback: 'angular.callbacks._'+callbackint,
-                  location: '20009',
-                  oauth_consumer_key: 'YXGa4ru-gTal2YshH1sA8A', //Consumer Key
-                  oauth_token: 'gmiTY407KpN0U4qdU2ea9BgJTXvianPF', //Token
-                  oauth_signature_method: "HMAC-SHA1",
-                  oauth_timestamp: new Date().getTime(),
-                  oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
-                  term: 'food'
-              };
-          var consumerSecret = 'bBHFOWAQyzK8JDTmBGnacSioY6c'; //Consumer Secret
-          var tokenSecret = 'bR6DViXqQNm7Pu9JdUWxDWUWD2s'; //Token Secret
-          var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, { encodeSignature: false});
-          params['oauth_signature'] = signature;
-          $http.jsonp(url, {params: params}).success(callback);
+      "processText": function(textForProcessing) {
+          $http.post('https://api.textrazor.com', {
+            apiKey: 'e283e70a509f0818b7ecbe9957b9def2f71e039105538f6fa807c684',
+            text: textForProcessing,
+            extractors: 'topics,phrases,words,dependency-trees'
+        }).
+        success(function(data, status, headers, config) {
+            return data;
+        }).
+        error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            return "Oops! Something went wrong.";
+        });
       }
   }
-})
-.factory("MyYelpBusiness", function($http) {
-  function randomString(length, chars) {
-    var result = '';
-    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
-    return result;
-  }
-  return {
-      "retrieveYelp": function(name, callback, callbackint) {
-          var method = 'GET';
-          var request_url = 'http://api.yelp.com/v2/business/'+name;
-          var parameters = {
-                  oauth_consumer_key: 'YXGa4ru-gTal2YshH1sA8A', //Consumer Key
-                  oauth_token: 'gmiTY407KpN0U4qdU2ea9BgJTXvianPF', //Token
-                  oauth_signature_method: "HMAC-SHA1",
-                  oauth_timestamp: new Date().getTime(),                  
-                  oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-              };
-          var consumerSecret = 'bBHFOWAQyzK8JDTmBGnacSioY6c'; //Consumer Secret
-          var tokenSecret = 'bR6DViXqQNm7Pu9JdUWxDWUWD2s'; //Token Secret
-          var signature = oauthSignature.generate(method, request_url, parameters, consumerSecret, tokenSecret, { encodeSignature: false});
-          parameters['oauth_signature'] = signature;
-          var urlstring = 'http://api.yelp.com/v2/business/'+name+'?';
-          for(var x in parameters){
-            if (parameters.hasOwnProperty(x)) {
-              urlstring+= x + '=' + parameters[x] + '&';
-            }
-          }
-          urlstring = urlstring.substring(0, urlstring.length-1);
-          $.jsonp({
-            url: urlstring, // any JSON endpoint
-            corsSupport: false, // if URL above supports CORS (optional)
-            jsonpSupport: true, // if URL above supports JSONP (optional)
-            data: parameters, 
-            success: callback,
-            error: function(data, boo, bah){
-              console.log(data +' ' + boo+' '+bah);
-              console.log(data);
-            }
-            // error, etc.
-          });
-          //$http({url: request_url, method: 'GET', params: parameters}).success(function(data, status, headers, config){return data}).error(function(data, boo){console.log(data, boo)});
-      }
-  }
-})
-.factory("IncrementTheShit", function($http) {
-  var x = -1;
-  return {
-    addone: function() {
-      x = (x+1)%10;
-    },
-    get: function() {
-      console.log(x);
-      return x;
-    },
-  }
-})
+});
