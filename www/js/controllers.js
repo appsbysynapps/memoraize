434f8266f20ae08de8902ce20692cd46c1662192
Akhil Waghmare <Akhil@Akhils-MacBook-Pro.local>
2015-05-17 03:06:44 -0400

    hopefully photolibrary works

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,96 +1,94 @@
 angular.module('starter.controllers', ["firebase"])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout, Camera, $firebaseArray, NewOCRAPI) {
-    // Form data for the login modal
-    $scope.loginData = {};
+        // Form data for the login modal
+        $scope.loginData = {};
 
-    // Create the login modal that we will use later
-    $ionicModal.fromTemplateUrl('templates/login.html', {
-        scope: $scope
-    }).then(function(modal) {
-        $scope.modal = modal;
-    });
-
-    // Triggered in the login modal to close it
-    $scope.closeLogin = function() {
-        $scope.modal.hide();
-    };
-
-    // Open the login modal
-    $scope.login = function() {
-        $scope.modal.show();
-    };
-
-    // Perform the login action when the user submits the login form
-    $scope.doLogin = function() {
-        console.log('Doing login', $scope.loginData);
-
-        // Simulate a login delay. Remove this and replace with your login
-        // code if using a login system
-        $timeout(function() {
-            $scope.closeLogin();
-        }, 1000);
-    };
-
-    $ionicModal.fromTemplateUrl('templates/newDeck.html', {
-        scope: $scope,
-        animation: 'slide-in-up'
-    }).then(function(modal) {
-        $scope.newDeckModal = modal;
-    });
-
-
-    $scope.newDeck = function() {
-        $scope.newDeckModal.show();
-    };
-
-    $scope.closeNewDeck = function() {
-        $scope.newDeckModal.hide();
-    };
-
-    $scope.cooltext = 'Placeholder.';
-
-    $scope.getPhoto = function() {
-        console.log('Getting camera');
-        Camera.getPicture({
-            quality: 75,
-            //targetWidth: 320,
-            //targetHeight: 320,
-            //encodingType: Camera.EncodingType.JPEG,
-            saveToPhotoAlbum: false
-        }).then(function(imageURI) {
-            console.log(imageURI);
-            $scope.lastPhoto = imageURI;
-            $scope.cooltext = JSON.stringify(NewOCRAPI.getTextFromPhoto(imageURI));
-            alert($scope.cooltext);
-        }, function(err) {
-            alert(JSON.stringify(err));
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+        }).then(function(modal) {
+            $scope.modal = modal;
         });
-    };
 
-    $scope.getExistingPhoto = function() {
-        $scope.getPicture = function() {
-            var options = {
+        // Triggered in the login modal to close it
+        $scope.closeLogin = function() {
+            $scope.modal.hide();
+        };
+
+        // Open the login modal
+        $scope.login = function() {
+            $scope.modal.show();
+        };
+
+        // Perform the login action when the user submits the login form
+        $scope.doLogin = function() {
+            console.log('Doing login', $scope.loginData);
+
+            // Simulate a login delay. Remove this and replace with your login
+            // code if using a login system
+            $timeout(function() {
+                $scope.closeLogin();
+            }, 1000);
+        };
+
+        $ionicModal.fromTemplateUrl('templates/newDeck.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function(modal) {
+            $scope.newDeckModal = modal;
+        });
+
+
+        $scope.newDeck = function() {
+            $scope.newDeckModal.show();
+        };
+
+        $scope.closeNewDeck = function() {
+            $scope.newDeckModal.hide();
+        };
+
+        $scope.cooltext = 'Placeholder.';
+
+        $scope.getPhoto = function() {
+            console.log('Getting camera');
+            Camera.getPicture({
                 quality: 75,
-                destinationType: Camera.DestinationType.FILE_URI,
-                sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
-                encodingType: Camera.EncodingType.JPEG,
-                popoverOptions: CameraPopoverOptions
-            };
-
-            $cordovaCamera.getPicture(options).then(function(imageData) {
-                $scope.imgURI = imageData;
+                //targetWidth: 320,
+                //targetHeight: 320,
+                //encodingType: Camera.EncodingType.JPEG,
+                saveToPhotoAlbum: false
+            }).then(function(imageURI) {
+                console.log(imageURI);
+                $scope.lastPhoto = imageURI;
+                $scope.cooltext = JSON.stringify(NewOCRAPI.getTextFromPhoto(imageURI));
+                alert($scope.cooltext);
             }, function(err) {
-                console.log("err accessing photo library", err)
+                alert(JSON.stringify(err));
             });
-        }
-    }
+        };
 
-    var ref = new Firebase("https://memoraize.firebaseio.com/decks");
-    $scope.decks = $firebaseArray(ref);
+        $scope.getExistingPhoto = function() {
+            Camera.getPicture({
+                quality: 75,
+                sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+                //targetWidth: 320,
+                //targetHeight: 320,
+                //encodingType: Camera.EncodingType.JPEG,
+                saveToPhotoAlbum: false
+            }).then(function(imageURI) {
+                console.log(imageURI);
+//                $scope.lastPhoto = imageURI;
+//                $scope.cooltext = JSON.stringify(NewOCRAPI.getTextFromPhoto(imageURI));
+//                alert($scope.cooltext);
+            }, function(err) {
+                alert(JSON.stringify(err));
+            });
+        };
 
-    $scope.deck = {};
-    $scope.cards = {};
+    var ref = new Firebase("https://memoraize.firebaseio.com/decks"); $scope.decks = $firebaseArray(ref);
+
+    $scope.deck = {}; $scope.cards = {};
 
     $scope.saveDeck = function() {
 
